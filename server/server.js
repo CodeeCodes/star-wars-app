f4e36cbca1fd22d7459d405189cc3e89742e8dea
Codee Palmer <codeejpalmer@gmail.com>
2019-12-31 13:52:06 -0800

    working on server functionality

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
+var dotenv = require("dotenv");
 /// Middleware
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +23,7 @@ mongoose
   })
   .then(() => console.log("mongoDB is connected"))
   .catch(err => console.log(err));
-  
+
 //routes
 const authRouter = require("./routes/api/auth");
 const loginRouter = require("./routes/api/login");
